925c4c769cf053cc9b9591c0b349dd8ea1f9b65b
Kenichi Kamiya <kachick1@gmail.com>
2017-04-07 18:54:45 +0900

    Update ruby-install

--- vagrant_scripts/bootstrap.sh
+++ vagrant_scripts/bootstrap.sh
@@ -54,9 +54,9 @@ echo 'source /usr/local/share/chruby/auto.sh' >> ~/.bashrc
 
 # ruby-install
 pushd /tmp
-wget -O ruby-install-0.6.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz
-tar -xzvf ruby-install-0.6.0.tar.gz
-pushd ruby-install-0.6.0/
+wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
+tar -xzvf ruby-install-0.6.1.tar.gz
+pushd ruby-install-0.6.1/
 sudo make install
 popd
 popd
